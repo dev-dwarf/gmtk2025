c0188e317314d64d4e6675187d61e459d3f26133
dev_dwarf <logan.c.forman@gmail.com>
2025-07-31 22:25:14 -0600

    reorg code

--- scripts/player.gd
+++ scripts/player.gd
@@ -25,58 +25,42 @@ var coins = 0
 @onready var model = $Character
 @onready var animation = $Character/AnimationPlayer
 
-# Functions
-
 func _physics_process(delta):
+	var input := Vector3.ZERO
 
-	# Handle functions
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
 
-	handle_controls(delta)
-	handle_gravity(delta)
+	input = input.rotated(Vector3.UP, view.rotation.y)
 
-	handle_effects(delta)
+	if input.length() > 1:
+		input = input.normalized()
 
-	# Movement
+	movement_velocity = input * movement_speed * delta
 
-	var applied_velocity: Vector3
+	if Input.is_action_just_pressed("jump"):
+		if jump_single or jump_double:
+			Audio.play("res://sounds/jump.ogg")
 
-	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
-	applied_velocity.y = -gravity
+			gravity = -jump_strength
 
-	velocity = applied_velocity
-	move_and_slide()
+			model.scale = Vector3(0.5, 1.5, 0.5)
 
-	# Rotation
-
-	if Vector2(velocity.z, velocity.x).length() > 0:
-		rotation_direction = Vector2(velocity.z, velocity.x).angle()
-
-	rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
-
-	# Falling/respawning
-
-	if position.y < -10:
-		get_tree().reload_current_scene()
-
-	# Animation for scale (jumping and landing)
-
-	model.scale = model.scale.lerp(Vector3(1, 1, 1), delta * 10)
-
-	# Animation when landing
-
-	if is_on_floor() and gravity > 2 and !previously_floored:
-		model.scale = Vector3(1.25, 0.75, 1.25)
-		Audio.play("res://sounds/land.ogg")
-
-	previously_floored = is_on_floor()
-
-# Handle animation(s)
-
-func handle_effects(delta):
+			if jump_single:
+				jump_single = false;
+				jump_double = true;
+			else:
+				jump_double = false;
+				
+	gravity += 25 * delta
+	if gravity > 0 and is_on_floor():
+		jump_single = true
+		gravity = 0
 
 	particles_trail.emitting = false
 	sound_footsteps.stream_paused = true
-
+	
+	# Animation
 	if is_on_floor():
 		var horizontal_velocity = Vector2(velocity.x, velocity.z)
 		var speed_factor = horizontal_velocity.length() / movement_speed / delta
@@ -95,63 +79,33 @@ func handle_effects(delta):
 			animation.play("idle", 0.1)
 	elif animation.current_animation != "jump":
 		animation.play("jump", 0.1)
+	model.scale = model.scale.lerp(Vector3(1, 1, 1), delta * 10)
 
-# Handle movement input
+	
+	var applied_velocity: Vector3
 
-func handle_controls(delta):
+	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
+	applied_velocity.y = -gravity
 
-	# Movement
+	velocity = applied_velocity
+	move_and_slide()
 
-	var input := Vector3.ZERO
+	if Vector2(velocity.z, velocity.x).length() > 0:
+		rotation_direction = Vector2(velocity.z, velocity.x).angle()
 
-	input.x = Input.get_axis("move_left", "move_right")
-	input.z = Input.get_axis("move_forward", "move_back")
+	rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
 
-	input = input.rotated(Vector3.UP, view.rotation.y)
+	# Die
+	if position.y < -10:
+		get_tree().reload_current_scene()
 
-	if input.length() > 1:
-		input = input.normalized()
 
-	movement_velocity = input * movement_speed * delta
+	if is_on_floor() and gravity > 2 and !previously_floored:
+		model.scale = Vector3(1.25, 0.75, 1.25)
+		Audio.play("res://sounds/land.ogg")
 
-	# Jumping
-
-	if Input.is_action_just_pressed("jump"):
-
-		if jump_single or jump_double:
-			jump()
-
-# Handle gravity
-
-func handle_gravity(delta):
-
-	gravity += 25 * delta
-
-	if gravity > 0 and is_on_floor():
-
-		jump_single = true
-		gravity = 0
-
-# Jumping
-
-func jump():
-
-	Audio.play("res://sounds/jump.ogg")
-
-	gravity = -jump_strength
-
-	model.scale = Vector3(0.5, 1.5, 0.5)
-
-	if jump_single:
-		jump_single = false;
-		jump_double = true;
-	else:
-		jump_double = false;
-
-# Collecting coins
+	previously_floored = is_on_floor()
 
 func collect_coin():
-
 	coins += 1
-
 	coin_collected.emit(coins)
